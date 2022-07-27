957686049b60bc058c540faf5a368fdb79a2db18
Testlea <109687280+Testlea@users.noreply.github.com>
2022-07-27 21:27:43 +0530

    Add files via upload

---
+++ Twoheeler.java
@@ -0,0 +1,22 @@
+package Week1;
+public class Twoheeler {
+	public static void main(String[] args) {
+		int noOfWeels= 2;
+		short noOfMirrors= 2;
+		boolean isPunctured=true;
+		long chasisisNumber=123456;
+		String bikeName="Fz";
+		double runningKm=1578;
+		
+		System.out.println("noOfWheels"+ noOfWeels);
+		System.out.println("noOfMiirrors"+noOfMirrors);
+		System.out.println("isPunctured"+isPunctured);
+		System.out.println("chasisisNumber"+chasisisNumber);
+	    System.out.println("bikeName"+ bikeName);
+        System.out.println("runningKm"+runningKm);
+        
+       
+	}
+	
+}
+  
