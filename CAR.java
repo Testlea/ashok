d75acf971f8c06e78a6213ba6ef322c457c110b4
Testlea <109687280+Testlea@users.noreply.github.com>
2022-07-27 21:25:10 +0530

    Assi

---
+++ CAR.java
@@ -0,0 +1,33 @@
+package Week1;
+
+public class CAR {
+	public void turnonlight() { 
+		
+		System.out.println("turnonlight");
+	}
+	public void turnonindicator() {
+		System.out.println("turnonindicator");
+	}
+	public void turnonfm() {
+		System.out.println("turnonfm");
+	}
+	public void applubrake() {
+		System.out.println("applybrake");
+	}
+	public static void main (String [] args) {
+		CAR cr=new CAR();
+		cr. turnonlight();
+		cr. turnonindicator();
+		cr. turnonfm();
+		cr. applybrake();
+		
+
+	
+
+	}
+	private void applybrake() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
